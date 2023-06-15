738d1d3898c6047cdf6fa1bd71f1f4c7daef822e
IvanOrsh <ksironov@mail.ru>
2023-06-15 20:19:54 +0300

    refactor to testDb

---
+++ src/utils/testDb.ts
@@ -0,0 +1,45 @@
+import request from 'supertest';
+
+import { Database } from '../database';
+import { dbConfig } from '../config/database';
+import App from '../app';
+
+type Options = {
+  email?: string;
+  password?: string;
+  endpoint?: string;
+};
+
+export default class TestDb {
+  private db: Database;
+
+  constructor() {
+    this.db = new Database('test', dbConfig);
+  }
+
+  async start(): Promise<void> {
+    await this.db.connect();
+  }
+
+  async sync(): Promise<void> {
+    await this.db.sync();
+  }
+
+  async stop(): Promise<void> {
+    await this.db.disconnect();
+  }
+
+  getApp() {
+    return new App().getApp();
+  }
+
+  async registerUser(options: Options = {}) {
+    const {
+      email = 'test@test.com',
+      password = 'Test123',
+      endpoint = '/v1/register',
+    } = options;
+
+    return request(this.getApp()).post(endpoint).send({ email, password });
+  }
+}
