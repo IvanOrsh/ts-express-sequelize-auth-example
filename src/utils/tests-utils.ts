c102a98cd1594ffb6c6046af42b6eec9dd0e05f3
IvanOrsh <ksironov@mail.ru>
2023-06-15 18:35:59 +0300

    add registerUser test helper

--- src/utils/tests-utils.ts
+++ src/utils/tests-utils.ts
@@ -1,8 +1,15 @@
+import request from 'supertest';
+
 import '../config'; // load evn variables
 import { Database } from '../database';
 import { dbConfig } from '../config/database';
-import { environment } from '../config/environment';
 import App from '../app';
+
+type Options = {
+  email?: string;
+  password?: string;
+  endpoint?: string;
+};
 
 let db: Database;
 
@@ -22,4 +29,14 @@ export async function syncDb(): Promise<void> {
 
 export function getApp() {
   return new App().getApp();
+}
+
+export async function registerUser(options: Options = {}) {
+  const {
+    email = 'test@test.com',
+    password = 'Test123',
+    endpoint = '/v1/register',
+  } = options;
+
+  return request(getApp).post(endpoint).send({ email, password });
 }
