c4ddce2625d5fd6455457607325545204acc6e1f
IvanOrsh <ksironov@mail.ru>
2023-06-15 16:27:12 +0300

    refactor register controller to use regiserService

--- src/controllers/v1/register.ts
+++ src/controllers/v1/register.ts
@@ -1,9 +1,5 @@
 import { Request, Response, Router } from 'express';
-import { User, Role, UserRole, RefreshToken } from '../../models';
-import {
-  generateAccessToken,
-  generateRefreshToken,
-} from '../../utils/jwt-utils';
+import { RegisterService } from '../../services/regiserService';
 import { runAsyncWrapper } from '../../utils/runAsyncWrapper';
 
 const router = Router();
@@ -14,44 +10,12 @@ router.post(
   runAsyncWrapper(async (req: Request, res: Response) => {
     const { email, password, roles } = req.body;
 
-    const user = await User.findOne({ where: { email } });
-
-    if (user) {
-      return res.status(200).json({
-        success: false,
-        message: 'User already exists',
-      });
-    }
-
-    const transaction = await User.sequelize!.transaction();
-
     try {
-      const newUser = await User.create({ email, password } as any, {
-        transaction,
-      });
-      const jwtPayload = {
-        email: newUser.getDataValue('email'),
-        password: newUser.getDataValue('password'),
-      };
-      const accessToken = generateAccessToken(jwtPayload);
-      const refreshToken = generateRefreshToken(jwtPayload);
-      await RefreshToken.create(
-        {
-          token: refreshToken,
-          userId: newUser.id,
-        } as any,
-        { transaction }
+      const { accessToken, refreshToken } = await RegisterService.registerUser(
+        email,
+        password,
+        roles
       );
-
-      if (roles && Array.isArray(roles)) {
-        const rolesToSave = [];
-
-        for (const role of roles) {
-          const newRole = await Role.create({ role } as any, { transaction });
-          await newUser.$add('role', newRole, { transaction });
-        }
-      }
-      await transaction.commit();
 
       return res.json({
         success: true,
@@ -63,7 +27,6 @@ router.post(
       });
     } catch (err) {
       const error = err as Error;
-      await transaction.rollback();
       return res.status(500).json({
         success: false,
         message: error.message,
