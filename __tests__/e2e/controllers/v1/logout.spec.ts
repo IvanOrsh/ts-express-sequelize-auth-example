21c39317e2ae2efad7f559df2a97e50d27719a4a
IvanOrsh <ksironov@mail.ru>
2023-06-16 19:22:13 +0300

    add e2e test for logout controller

--- __tests__/e2e/controllers/v1/logout.spec.ts
+++ __tests__/e2e/controllers/v1/logout.spec.ts
@@ -1,8 +1,8 @@
 import request from 'supertest';
 import TestDb from '../../../test-helpers/testDb';
-import { RefreshToken } from '../../../../src/models';
+import { RefreshToken, User } from '../../../../src/models';
 
-describe('token controller', () => {
+describe('logout controller', () => {
   const testDb = new TestDb();
   const app = testDb.getApp();
   let newUserResponse: request.Response;
@@ -25,7 +25,38 @@ describe('token controller', () => {
     newUserResponse = await testDb.registerUser(userData);
   });
 
-  test('test', () => {
-    expect(true).toBeTruthy();
+  describe('requiresAuth middleware', () => {
+    test('should fail if the access token is invalid', async () => {
+      const response = await request(app)
+        .post('/v1/logout')
+        .set('Authorization', `Bearer invalid.token`)
+        .send()
+        .expect(401);
+
+      const { success, message } = response.body;
+
+      expect(success).toBeFalsy();
+      expect(message).toBe('Invalid token');
+    });
+  });
+
+  test('should successfully logout a user when provided with valid access token', async () => {
+    const accessToken = newUserResponse.body.data.accessToken;
+    const response = await request(app)
+      .post('/v1/logout')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send()
+      .expect(200);
+
+    const { success, message } = response.body;
+
+    expect(success).toBeTruthy();
+    expect(message).toBe('Successfully logged out');
+
+    const user = await User.findOne({
+      where: { email: userData.email },
+      include: RefreshToken,
+    });
+    expect(user!.refreshToken.getDataValue('token')).toEqual(null);
   });
 });
